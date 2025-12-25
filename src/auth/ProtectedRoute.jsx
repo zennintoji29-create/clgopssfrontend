9ce2ad0e24a5a5fad81cbcc0e7df0acc32c4b7cf
subhansumain <zennintoji29@gmail.com>
2025-12-25 16:57:06 +0530

    Update ProtectedRoute.jsx

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -2,11 +2,13 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = ({ children, role }) => {
-  const { user, loading } = useAuth();
+  const { user } = useAuth();
 
-  if (loading) return null;
   if (!user) return <Navigate to="/login" replace />;
-  if (role && user.role !== role) return <Navigate to="/" replace />;
+
+  if (role && user.role !== role) {
+    return <Navigate to="/" replace />;
+  }
 
   return children;
 };
