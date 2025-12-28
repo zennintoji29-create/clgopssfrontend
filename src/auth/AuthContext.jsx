33a158fd258f12c5245d2e3a6705adf7da05f6f0
subhansumain <zennintoji29@gmail.com>
2025-12-28 20:14:03 +0530

    Update AuthContext.jsx

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,30 +1,33 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import api from "../api/axios";
 
-const AuthContext = createContext();
+const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // Load user on refresh
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
-    const token = localStorage.getItem("accessToken");
+    const accessToken = localStorage.getItem("accessToken");
 
-    if (storedUser && token) {
+    if (storedUser && accessToken) {
       setUser(JSON.parse(storedUser));
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
+      api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
     }
+
     setLoading(false);
   }, []);
 
   const login = async (email, password) => {
     const res = await api.post("/auth/login", { email, password });
 
-    const { user, accessToken } = res.data.data;
+    const { user, accessToken, refreshToken } = res.data.data;
 
-    localStorage.setItem("accessToken", accessToken);
     localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
 
     api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
     setUser(user);
